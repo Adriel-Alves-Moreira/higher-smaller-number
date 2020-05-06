fd00b9f2676a0b3d3440f4adddb2547b99825ba8
Adriel Alves Moreira <63890536+Adriel-Alves-Moreira@users.noreply.github.com>
2020-05-06 13:29:27 -0300

    Add files via upload

---
+++ higher-smaller.py
@@ -0,0 +1,8 @@
+n1 = int(input("Type a number: "))
+n2 = int(input("Type another number: "))
+if n1 > n2:
+    print("The first number is higher")
+elif n1 < n2:
+    print("The second number is higher")
+elif n1 == n2:
+    print("There is no greater value, the two are equal")
